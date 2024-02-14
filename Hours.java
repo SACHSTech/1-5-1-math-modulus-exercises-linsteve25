fbb2be52ae56af1be8b890777628ba748c4593a4
linsteve25 <steve.lin25@ycdsbk12.ca>
2024-02-14 14:21:34 -0500

    modified:   Hours.java

--- Hours.java
+++ Hours.java
@@ -1,13 +1,28 @@
 class Hours extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * A program that converts given hours into days and hours
+  * @author: Steve Lin 
   */
   
   public void run() {
     
-    // start coding here
+    // declare variables
+    int intHours;
+    int intDays;
+    int intRemainder;
+
+    // get hours;
+    intHours = readInt ("Enter the number of hours: ");
+
+    // calculate number of days and hours
+    intDays = intHours / 24;
+    intRemainder = intHours % 24;
+
+    // output results
+    System.out.println ("There are " + intDays + " days and " + intRemainder + " hours in " + intHours + " hours. ");
+    
+
     
   }
 }
