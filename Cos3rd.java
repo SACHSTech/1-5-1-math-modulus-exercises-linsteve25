d00ad6b40a67f1edb2ecafeddff3c2a3dc6f6625
linsteve25 <steve.lin25@ycdsbk12.ca>
2024-02-14 22:34:52 -0500

    modified:   Cos3rd.java

--- Cos3rd.java
+++ Cos3rd.java
@@ -1,13 +1,29 @@
 class Cos3rd extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * A program that calculates the missing third side through cosine law
+  * @author: Steve Lin 
   */
   
   public void run() {
     
-    // start coding here
+    // declare variables 
+    double dblAngle;
+    double dblSideA;
+    double dblSideB;
+    double dblSideC;
+
+    // enter sides and angle
+    dblAngle = readDouble("Enter the angle: ");
+    dblSideA = readDouble("Enter the first side length: ");
+    dblSideB = readDouble("Enter the second side lenght: ");
+
+    // calculate missing side
+    dblSideC = Math.sqrt(Math.pow (dblSideA, 2) + Math.pow (dblSideB, 2) - 2 * dblSideA * dblSideB * Math.cos(Math.toRadians(dblAngle)));
+
+    // output result
+    System.out.println ("The length of the missing side is: " + dblSideC);
+
     
   }
 }
