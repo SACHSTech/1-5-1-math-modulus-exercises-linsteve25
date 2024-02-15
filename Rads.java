ecd81b319790aa88d1888ae3b7f861989007e799
linsteve25 <steve.lin25@ycdsbk12.ca>
2024-02-14 21:55:11 -0500

    modified:   Rads.java

--- Rads.java
+++ Rads.java
@@ -1,13 +1,25 @@
 class Rads extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * A program that converts degree measures to radian measures
+  * @author: Steve Lin
   */
   
   public void run() {
     
-    // start coding here
+    // declare variables
+    double dblDegrees;
+    double dblRadians;
+
+    // get degrees
+    dblDegrees = readDouble("Enter the degrees: ");
+
+    // convert degrees to radians
+    dblRadians = Math.toRadians(dblDegrees);
+
+    // outpute result
+    System.out.println (dblDegrees + " degrees converted to radians is " + dblRadians);
     
   }
+
 }
