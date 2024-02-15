bbaf722000d760d435216523fb90cf5bc0de1023
linsteve25 <steve.lin25@ycdsbk12.ca>
2024-02-14 19:12:01 -0500

    modified:   Minutes.java

--- Minutes.java
+++ Minutes.java
@@ -1,13 +1,35 @@
 class Minutes extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * A program that computes the number of days, hours, and minutes from minutes given by the user
+  * @author: Steve Lin 
   */
   
   public void run() {
     
-    // start coding here
+    // declare variables
+    int intMinutes;
+    int intRemainderMinutes;
+    int intHours;
+    int intRemainderHours;
+    int intDays;
+
+    // get minutes
+    intMinutes = readInt ("Enter the number of minutes: ");
+
+    // compute number of days, hours, and minutes
+    intHours = intMinutes / 60;
+    intRemainderMinutes = intMinutes % 60;
+
+    intDays = intHours / 24;
+    intRemainderHours = intHours % 24;
+
+    // output results
+    System.out.println ("In " + intMinutes + " minutes there are " + intDays + " days, " + intRemainderHours + " hours, and " + intRemainderMinutes + " minutes.");
+
+
+    
+
     
   }
 }
